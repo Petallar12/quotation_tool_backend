b336b3422b6ce51cff881da52e5aea174ebd7301
PH-Strawhat <139205442+PH-Strawhat@users.noreply.github.com>
2025-01-08 12:06:23 +0800

    change server.js cors addapp.use(cors({   origin: 'https://lukemedikal.co.id' // Replace with your frontend domain }));

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://lukemedikal.co.id' // Replace with your frontend domain
+}));
 app.use(bodyParser.json());
 
 // Nodemailer transporter configuration
