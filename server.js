04dc4517db7216e65b027212561f487ac4cc316e
PH-Strawhat <139205442+PH-Strawhat@users.noreply.github.com>
2025-01-08 12:52:22 +0800

    balik muna sa dati ang cors

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://lukemedikal.co.id' // Replace with your frontend domain
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Nodemailer transporter configuration
